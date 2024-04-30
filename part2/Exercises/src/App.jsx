f31280259104ef06f0fcc52616bc9b925ab068af
manulucena <mlucenasalas@gmail.com>
2024-04-30 14:07:09 +0200

    Exercise 2.4

--- part2/Exercises/src/App.jsx
+++ part2/Exercises/src/App.jsx
@@ -1,57 +1,74 @@
-const Course = ({course, total}) => {
-  return(
-    <>
-      <h1 key={course.id}> {course.name} </h1>
+const App = () => {
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+  return (
+    <div>
+      <h1 key={courses[0].id} >Course name: {courses[0].name} </h1>
       <ul>
-        {course.parts.map((parts)=>{
-          return(
-            <li key={parts.id}>Part name: {parts.name}. Numer of exercises: {parts.exercises}</li>
+        {courses[0].parts.map((part)=>{
+          return (
+            <li key={part.id}>Part name: {part.name}. Number of exercises: {part.exercises}  </li>
           )
         })}
       </ul>
-      <h2>Total number of exercises: {total} </h2>
-      <h2>Total number of exercises using .reduce: {course.parts.reduce((total, parts)=>{
-        return total + parts.exercises
+      <h2>Total number of exercises: {courses[0].parts.reduce((total,part)=>{
+        return total + part.exercises
       },0)} </h2>
-    </>
+      <h1 key={courses[1].id}>Enviroment: {courses[1].name} </h1>
+      <ul>
+        {courses[1].parts.map((part)=>{
+          return(
+            <li  key={part.id}>Name: {part.name}. Number of exercises: {part.exercises}  </li>
+          )
+        })}
+      </ul>
+      <h2>Total number of exercises: {courses[1].parts.reduce((total2, part2)=>{
+        return total2 + part2.exercises
+      },0)} </h2>
+    </div>
   )
 }
-const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
-  let total = 0
-  let i = 0
-  for(i=0;i<3;i++){
-    total += course.parts[i].exercises
-  }
-  course.parts.reduce((total, parts) =>{
-    return total + parts.exercises
-  }, 0)
-
-  return (
-    <>
-      <Course course={course} total={total} />
-    </>
-  )
-}
-
 export default App
