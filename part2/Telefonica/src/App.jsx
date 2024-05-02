9c498ea6f5c831dbbf25194e4ccf667eebef33ae
manulucena <mlucenasalas@gmail.com>
2024-05-02 15:15:08 +0200

    Exercise 2.11

--- part2/Telefonica/src/App.jsx
+++ part2/Telefonica/src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react"
+import { useEffect } from "react"
+import axios from 'axios'
 const AddPerson = ({persons, setPersons}) => {
     const [newName, setNewName] = useState('')
     const handleChange = (event) =>{
@@ -81,12 +83,14 @@ const List = ({persons})=> {
     )
 }
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', phone: '040-123456', id: 1 },
-        { name: 'Ada Lovelace', phone: '39-44-5323523', id: 2 },
-        { name: 'Dan Abramov', phone: '12-43-234345', id: 3 },
-        { name: 'Mary Poppendieck', phone: '39-23-6423122', id: 4 }
-    ]) 
+    const [persons, setPersons] = useState([]) 
+    useEffect(()=> {
+        axios
+        .get('http://localhost:3001/persons')
+        .then((response)=>{
+            setPersons(response.data)
+        })
+    },[])
     
       return (
         <div>
