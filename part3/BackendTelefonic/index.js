5256ac66c40896a7ef230cc35f4eee22ac7854fe
manulucena <mlucenasalas@gmail.com>
2024-05-14 08:17:10 +0200

    Exercise 3.9

--- part3/BackendTelefonic/index.js
+++ part3/BackendTelefonic/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require ('cors')
 const app = express()   
 app.use(express.json())
 app.use(morgan('tiny'))
+app.use(cors())
 function customPostFormat(tokens, req, res) {
     return JSON.stringify({
       method: tokens.method(req, res),
