2182f623b5d5348d427459a5e0e3dcbac0688d21
manulucena <mlucenasalas@gmail.com>
2024-05-20 14:32:14 +0200

    Exercise 2.20

--- part3/BackendTelefonic/modules/person.js
+++ part3/BackendTelefonic/modules/person.js
@@ -6,7 +6,17 @@ const personSchema = new mongoose.Schema({
         required: true,
         minlength: 3
     },
-    number: String
+    number: {
+        type: String,
+        required: true,
+        minlength: 8,
+        validate: {
+            validator: function(v) {
+                return /^\d{2,3}-\d+$/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        },
+    }
 })
 
 const Person = mongoose.model('person', personSchema)
