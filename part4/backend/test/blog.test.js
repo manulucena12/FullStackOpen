4642fa83a36f725c7e25c2283f38f350e1d87e61
manulucena <mlucenasalas@gmail.com>
2024-05-22 16:32:26 +0200

    Exercise 4.4

--- part4/backend/test/blog.test.js
+++ part4/backend/test/blog.test.js
@@ -2,13 +2,44 @@ const supertest = require('supertest')
 const {app,server} = require('../index')
 const { default: mongoose } = require('mongoose')
 const api = supertest(app)
-const {dummy} = require('../utils/list_helper')
+const {dummy, totalLikes} = require('../utils/list_helper')
 
 test('Checking number of blogs', ()=>{
     const blogs = []
     const result = dummy(blogs)
     expect(result).toBe(1)
 })
+
+describe('Total Likes', ()=>{
+    const myBlogs = [
+        {
+          "title": "Donda",
+          "author": "Kanye West",
+          "url": "URl",
+          "likes": 3,
+          "id": "664c3586936f68cc996157a3"
+        },
+        {
+          "title": "Pedro",
+          "author": "Manu",
+          "url": "URll",
+          "likes": 4,
+          "id": "664df8007c9dc9dcdf8c91ec"
+        },
+        {
+          "title": "Pedro",
+          "author": "Manu",
+          "url": "URll",
+          "likes": 4,
+          "id": "664df8626540763654da491b"
+        }
+    ]
+    test('Checking if the total of likes is 11', ()=>{
+        const result = totalLikes(myBlogs)
+        expect(result).toBe(11)
+    })
+})
+
 
 afterAll(()=>{
     mongoose.connection.close()
--- part4/backend/utils/list_helper.js
+++ part4/backend/utils/list_helper.js
@@ -2,7 +2,15 @@ const dummy = (blogs) => {
     const numberBlogs = 1
     return numberBlogs
 }
-  
+
+const totalLikes = (blogs) =>{
+    const likes = blogs.map(blog=> blog.likes)
+    let total = 0
+    likes.forEach(like => total += like)
+    return total
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
