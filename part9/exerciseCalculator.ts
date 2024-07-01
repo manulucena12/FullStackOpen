9f136f2b77cb07a9d2ac1715dac3093b4e47b72d
manulucena <mlucenasalas@gmail.com>
2024-07-01 17:06:11 +0200

    Exercise 9.2

---
+++ part9/exerciseCalculator.ts
@@ -0,0 +1,41 @@
+import { Result } from "./types/exercises"
+
+const exerciseCalculator = (hours: Array<number>, goal: number) : Result => {
+    let trainingDays= 0
+    let success = true
+    let rating = 1
+    let ratingDescription = ''
+    const target = goal
+    let total = 0
+    for(let i = 0; i < hours.length ; i++){
+        if(hours[i] > 0 ) {
+            trainingDays += 1
+            rating += .285
+        }
+        if(hours[i] >= goal){
+            success = false
+        }
+        total += hours[i]
+    }
+    if(rating < 2) {
+        ratingDescription = 'You have to do more exercise!'
+    }else if(rating > 2 && rating < 2.6){
+        ratingDescription = 'Keep going!'
+    }else{
+        ratingDescription = 'Well done!'
+    }
+    const periodLength = hours.length
+    const average = total/hours.length
+    const result: Result = {
+        trainingDays,
+        success,
+        rating,
+        ratingDescription,
+        periodLength,
+        average,
+        target
+    }
+    return result
+}
+
+console.log(exerciseCalculator([3, 0, 2, 4.5, 0, 3, 1], 2))
