928dfd5e7ec50e244fdf6dc84426c37b8202f78c
manulucena <mlucenasalas@gmail.com>
2024-07-02 10:10:41 +0200

    Exercise 9.9

---
+++ part9/Patientor/Backend/Types/patient.ts
@@ -0,0 +1,14 @@
+export interface Patient {
+    id: string;
+    name: string;
+    occupation: string;
+    gender: Gender;
+    ssn?: string;
+    dateOfBirth?: string;
+}
+
+export enum Gender {
+    Male = "male",
+    Female = "female",
+    Other = "other"
+}
--- part9/Patientor/Backend/index.ts
+++ part9/Patientor/Backend/index.ts
@@ -1,12 +1,17 @@
 import express from "express"
 import cors from 'cors'
 import 'dotenv/config'
+import { getPatients } from "./Services/patients"
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 
-app.get('/api/ping', (req,res)=>{
-    res.send('Hola')
+app.get('/ping', (req,res)=>{
+    res.json('Hola')
+})
+
+app.listen(3002, ()=>{
+    console.log('App running properly')
 })
