453486f5a5ece2d43603a3770fbbab943929c358
manulucena <mlucenasalas@gmail.com>
2024-07-02 09:40:23 +0200

    Exercise 9.8

---
+++ part9/Patientor/Backend/index.ts
@@ -0,0 +1,12 @@
+import express from "express"
+import cors from 'cors'
+import 'dotenv/config'
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/api/ping', (req,res)=>{
+    res.send('Hola')
+})
